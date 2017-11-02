f39399ddf1a97e7b3429debf7bfdb9836366baae
OGMP-Gyrth <gyrthmcmulin@gmail.com>
2017-11-02 17:11:30 +0100

    Added therium animation search to python script.

--- update_animation_list.py
+++ update_animation_list.py
@@ -3,15 +3,22 @@ import os
 import xml.etree.ElementTree as etree
 from lxml import etree
 
-path = "/run/Hyperdisk/SteamLibraryLinux/steamapps/common/Overgrowth/Data/Animations"
+main_path = "/run/Hyperdisk/SteamLibraryLinux/steamapps/common/Overgrowth/Data/Animations"
+therium_path = "/run/Hyperdisk/SteamLibraryLinux/steamapps/workshop/content/25000/1126025778/Data/Custom/timbles/therium-2/Animations"
 index = 0
-
 root = etree.Element('root')
-for file in os.listdir(path):
-    if file.endswith(".anm"):
-        print(os.path.join("Data/Animations/", file))
-        element = etree.SubElement(root, "path", key="animation" + str(index), path=os.path.join("Data/Animations/", file))
-        index += 1
+
+def find_animations (path):
+    global index
+    global root
+    for file in os.listdir(path):
+        if file.endswith(".anm"):
+            print(os.path.join("Data/Animations/", file))
+            element = etree.SubElement(root, "path", key="animation" + str(index), path=os.path.join("Data/Animations/", file))
+            index += 1
+
+find_animations(main_path)
+find_animations(therium_path)
 
 tree = etree.ElementTree(root)
 tree.write("animation_browser_paths.xml", pretty_print=True, xml_declaration=True, encoding='utf-8', method="xml")
