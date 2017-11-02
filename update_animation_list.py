3a4dce2ffb69c966b54fd0249b182edc8504bf89
OGMP-Gyrth <gyrthmcmulin@gmail.com>
2017-11-02 17:20:39 +0100

    Pyhton script also searches sub directories now.

--- update_animation_list.py
+++ update_animation_list.py
@@ -6,19 +6,21 @@ from lxml import etree
 main_path = "/run/Hyperdisk/SteamLibraryLinux/steamapps/common/Overgrowth/Data/Animations"
 therium_path = "/run/Hyperdisk/SteamLibraryLinux/steamapps/workshop/content/25000/1126025778/Data/Custom/timbles/therium-2/Animations"
 index = 0
-root = etree.Element('root')
+root_element = etree.Element('root')
 
 def find_animations (path):
     global index
-    global root
-    for file in os.listdir(path):
-        if file.endswith(".anm"):
-            print(os.path.join("Data/Animations/", file))
-            element = etree.SubElement(root, "path", key="animation" + str(index), path=os.path.join("Data/Animations/", file))
-            index += 1
+    global root_element
+
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if name.endswith((".anm")):
+                print(os.path.join("Data/Animations/", name))
+                element = etree.SubElement(root_element, "path", key="animation" + str(index), path=os.path.join("Data/Animations/", name))
+                index += 1
 
 find_animations(main_path)
 find_animations(therium_path)
 
-tree = etree.ElementTree(root)
+tree = etree.ElementTree(root_element)
 tree.write("animation_browser_paths.xml", pretty_print=True, xml_declaration=True, encoding='utf-8', method="xml")
