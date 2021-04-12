3ecc084337babae30472f99c3d141420d4a17b9a
Gyrth <gyrthmcmulin@gmail.com>
2021-04-12 19:56:08 +0200

    Added furniture assets.

--- fix_texture_alpha.py
+++ fix_texture_alpha.py
@@ -6,7 +6,7 @@ from lxml import etree
 from PIL import Image, ImageDraw, ImageFilter
 from os import walk
 
-textures_path = "./kenney/Data/Textures/kenney/Pirate_Kit"
+textures_path = "./kenney/Data/Textures/kenney/Furniture_Kit"
 
 index = 0
 root_element = etree.Element('root')
