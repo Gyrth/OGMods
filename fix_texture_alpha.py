910f62dae707458d9de9517b7b9d5dd5abc328d2
Gyrth <gyrthmcmulin@gmail.com>
2021-04-12 13:34:46 +0200

    Added the holiday assets.

--- fix_texture_alpha.py
+++ fix_texture_alpha.py
@@ -6,7 +6,7 @@ from lxml import etree
 from PIL import Image, ImageDraw, ImageFilter
 from os import walk
 
-textures_path = "./kenney/Data/Textures/kenney/City_Kit_(Roads)"
+textures_path = "./kenney/Data/Textures/kenney/Holiday_Kit"
 
 index = 0
 root_element = etree.Element('root')
