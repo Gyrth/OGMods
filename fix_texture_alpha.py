e10eced303fd4f34348cde65d90e6f0d7fd2c2ee
Gyrth <gyrthmcmulin@gmail.com>
2021-04-12 20:36:35 +0200

    Added racing assets.

--- fix_texture_alpha.py
+++ fix_texture_alpha.py
@@ -6,7 +6,7 @@ from lxml import etree
 from PIL import Image, ImageDraw, ImageFilter
 from os import walk
 
-textures_path = "./kenney/Data/Textures/kenney/Castle_Kit"
+textures_path = "./kenney/Data/Textures/kenney/Racing_Kit"
 
 index = 0
 root_element = etree.Element('root')
